3c9381c8dc5a7297a9a91c551b2053a55c4a7bd6
danil <dan090905@gmail.com>
2025-12-08 20:27:40 +0300

    correcting the path

--- api/main.py
+++ api/main.py
@@ -3,6 +3,12 @@ from api.predict import load_artifacts, make_prediction
 from api.in_out import InputData, OutputData
 from model.custom_transformers import DropHighNaN, SplitObjectColumns, FillNumericMedian, KeepSelectedFeatures
 
+import sys
+sys.modules['__main__'].DropHighNaN = DropHighNaN
+sys.modules['__main__'].SplitObjectColumns = SplitObjectColumns
+sys.modules['__main__'].FillNumericMedian = FillNumericMedian
+sys.modules['__main__'].KeepSelectedFeatures = KeepSelectedFeatures
+
 app = FastAPI(title="Income Prediction API")
 
 preprocess, model, metadata = load_artifacts()
@@ -10,5 +16,5 @@ preprocess, model, metadata = load_artifacts()
 @app.post("/predict", response_model=OutputData)
 def predict(data: InputData):
     df = data.to_dataframe()
-    pred = make_prediction(df, preprocess, model, metadata)
+    pred = make_prediction(df, preprocess, model)
     return OutputData(prediction=float(pred[0]))
